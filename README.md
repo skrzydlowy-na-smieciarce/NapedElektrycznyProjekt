6d91c3e35630a5e93136dfc0b7aa89f988a6b7c5
jakub <jakub@jakub-Latitude-E6440>
2021-04-02 20:57:02 +0200

    Jak narazie wiekszosz wstepnie, bez komentarza

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NapedProjekt
+# Kaskadowa struktura regulacji napędu prądu stałego
 
 Moja wersja Matlaba: <b>  Matlab R2019b</b>
+
+<h2> TODO </h2>
+
--- dane.m
+++ dane.m
@@ -1,3 +1,34 @@
-% Dane projektu
-a=10;
-b="TEST";
+%% Dane projektu 
+% Plik, skrypt ten w założeniu ma się atomatycznie wczytywać po uruchomieniu
+% projektu
+
+% Ważne: ze wzgledu na kolizje oznaczeń zakomentować to co nie "idzie" do
+% simulinka
+
+%% Dane silnika
+Pn=17; % [kW]
+nn=1500; % [obr/min]
+Un=220; % [V]
+In=88; % [A]
+Js=0.275; % [kgm2]
+Rt=0.15; % [Ohm]
+Lt=18.75; % [mH]
+omega_n=nn*2*pi/60;
+psi_en=(Un-Rt*In)/omega_n; % [Wb]
+Mn=psi_en*In; % [Nm]
+% przyjete dane
+lambda=2;
+p=50;
+
+%% Regulatory ciągłe
+% założone dane
+Y=10/(2.5*In);
+Kt=10/(1.2*omega_n);
+Kp=0.15*Un;
+J=6; %[kgm2]
+% obliczenia potrzebne do simulinka
+regulatory_ciagle % uruchomienie skryptu regulatory_ciagle.m
+
+%% Regulatory dyskretne
+
+%% Przekształtnik tyrystorowy
