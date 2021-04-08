f2cbe4a5d7df24f5b826d7c97065b2ec33d88545
jakub <sjakub@outlook.com>
2021-04-09 00:06:13 +0200

    wykresy

---
+++ czwartek.m
@@ -0,0 +1,8 @@
+plot(out.tout, out.yout(:,1))
+plot(out.tout, out.yout(:,2))
+plot(out.tout, out.yout(:,2))
+plot(out.tout, out.yout(:,1))
+hold on
+plot(out.tout, out.yout(:,2))
+plot(out.tout, out.yout(:,3))
+legend("Prad", "napiecie", "predkosc")
